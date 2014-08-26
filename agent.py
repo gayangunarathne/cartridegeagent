df601d61bc36f50ed3d711bd820e68c353f80eb5
gayan <gayan@puppet.gayan.org>
2014-08-26 19:14:15 +0530

    updates

--- agent.py
+++ agent.py
@@ -80,6 +80,36 @@ def listeningInstanceNotifier():
 def onInstanceStartedEvent():
     print('on instance start up event')
 
+def publishInstanceStartedEvent():
+    class MyListener(stomp.ConnectionListener):
+        def on_error(self, headers, message):
+            print('received an error %s' % message)
+        def on_message(self, headers, message):
+            for k,v in headers.iteritems():
+                print('header: key %s , value %s' %(k,v))
+            print('received message\n %s'% message)
+
+
+    dest='/topic/instance-status'
+    conn=stomp.Connection([('localhost',61613)])
+    print('set up Connection')
+
+
+    conn.start()
+    print('started connection')
+
+    conn.connect(wait=True)
+    print('connected')
+    conn.subscribe(destination=dest, ack='auto')
+    print('subscribed')
+
+    message='hello cruel world'
+    conn.send(message=message, destination=dest,headers={'seltype':'mandi-age-to-man','type':'textMessage','MessageNumber':random.randint(0,65535)},ack='auto')
+    print('sent message')
+    time.sleep(2)
+    print('slept')
+    conn.disconnect()
+    print('disconnected')
 
 
 t1 = threading.Thread(target=runningSuspendScript, args=[])
@@ -95,4 +125,6 @@ t3 = threading.Thread(target=listeningTopology, args=[])
 t3.start()
 
 onInstanceStartedEvent()
+
+publishInstanceStartedEvent()
 
